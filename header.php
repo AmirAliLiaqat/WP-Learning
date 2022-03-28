d2c03cc25d35db61baffa8ac59883682fe05f8ff
Amir Ali <amirliaqat2020@gmail.com>
2022-03-28 17:52:24 +0500

    Adding wp_head and wp_footer functions

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <meta charset="<?php bloginfo('charset');?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo get_the_title(); ?> | <?php bloginfo('name')?></title>
+    <title><?php bloginfo('name')?></title>   
+    <?php wp_head(); ?>
 </head>
 <body>
