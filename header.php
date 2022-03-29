6bb77a0820ed23f9c28bc1f022ae327f551713ed
Amir Ali <amirliaqat2020@gmail.com>
2022-03-29 11:08:51 +0500

    Adding Custom logo and nav menu

--- header.php
+++ header.php
@@ -11,7 +11,22 @@
     <meta charset="<?php bloginfo('charset');?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name')?></title>   
+    <title><?php the_title(); ?> | <?php bloginfo('name')?></title>   
     <?php wp_head(); ?>
 </head>
 <body>
+
+    <div class="site-main container">
+        <header class="site-header">
+            <div class="site-branding">
+                <?php the_custom_logo(); ?>
+            </div><!--site-branding-->
+        </header>
+
+        <div class="site-navigation pt-3">
+            <?php 
+                wp_nav_menu( array (
+                    'theme_location' => 'primary'
+                ) ); 
+            ?>
+        </div><!--site-navigation-->
