c033c4aee6d2d8484740342b2f536765e8a69962
Amir Ali <amirliaqat2020@gmail.com>
2022-03-29 11:09:20 +0500

    Adding basic structure

--- index.php
+++ index.php
@@ -4,16 +4,28 @@
  *
  * @package wplearning
 */
+get_header();   
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Learning</title>
-</head>
-<body>
+
+    <div class="home-main">
+        <div class="row mx-0">
+            <div class="home-posts col-lg-8 col-md-8 col-sm-8 col-xs-12 pull-left">
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <h1><?php the_title(); ?></h1>
+                        <p><?php the_content(); ?></p>
+                    <?php  endwhile; ?>
+                <?php endif; ?>
+            </div><!--home-posts-->
+
+            <div class="home-sidebar col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
+                <?php get_sidebar(); ?>
+            </div><!--home-sidebar-->
+        </div><!--row-->
+    </div><!--home-main-->
+
+<?php
+get_sidebar();
+get_footer();
+?>
     
-</body>
-</html>
