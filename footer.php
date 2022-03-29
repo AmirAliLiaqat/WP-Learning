36a907fe5b1fabc107b08b8103a444cd69347df3
Amir Ali <amirliaqat2020@gmail.com>
2022-03-29 18:22:34 +0500

    Adding Footer Widgets

--- footer.php
+++ footer.php
@@ -6,6 +6,21 @@
 */
 ?>
 
+    <footer class="site-footer">
+        <div class="row mx-0 footer-widgets">
+            <div class="widget-col col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <?php dynamic_sidebar('footer-1'); ?>
+            </div><!--widget-->
+            <div class="widget-col col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <?php dynamic_sidebar('footer-2'); ?>
+            </div><!--widget-->
+            <div class="widget-col col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <?php dynamic_sidebar('footer-3'); ?>
+            </div><!--widget-->
+        </div><!--footer-widgets-->
+    </footer>
+</div><!--container-->
+
 <?php wp_footer(); ?>
 
 </body>
