30e3167fa5f947a01bc404fe22d008af3b0aee48
Amir Ali <amirliaqat2020@gmail.com>
2022-03-31 15:45:12 +0500

    Adding customiser style

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
+    <style type="text/css">
+        body{
+            background: <?php echo get_theme_mod('wplearning_body_bg_color', '#fff'); ?>;
+        }
+        .site-navigation{
+            background: <?php echo get_theme_mod('wplearning_nav_bg_color', '#2ca358'); ?>;
+        }
+    </style>
 </head>
 <body>
 
@@ -22,7 +30,7 @@
             </div><!--site-branding-->
         </header>
 
-        <div class="site-navigation pt-3">
+        <div class="site-navigation">
             <?php 
                 wp_nav_menu( array (
                     'theme_location' => 'primary'
